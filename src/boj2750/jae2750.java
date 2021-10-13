bb278da226eb585a25dbbd3cab3a083a934cf897
jakim09 <jakim09@naver.com>
2021-10-13 16:14:47 +0900

    [UPDATE] 재아 2주차 과제 제출

--- src/boj2750/jae2750.java
+++ src/boj2750/jae2750.java
@@ -1,4 +1,26 @@
 package boj2750;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class jae2750 {
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+ 
+		// 정렬 메소드
+		Arrays.sort(arr);
+		
+		for(int val : arr) {
+			System.out.println(val);
+		}
+ 
+	}
 }

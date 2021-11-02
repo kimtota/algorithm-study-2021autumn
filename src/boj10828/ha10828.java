a84296ed107816cbfb40dcd4029811294bc98d35
nhy31 <nahay131@gmail.com>
2021-11-02 21:46:46 +0900

    소스트리연습중..

---
+++ src/boj10828/ha10828.java
@@ -0,0 +1,16 @@
+package boj10828;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ha10828 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringBuilder sb = new StringBuilder();
+
+	}
+
+}

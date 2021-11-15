5f5d148cc90b0d9f1c48d212862b84b7e30059b4
nhy31 <nahay131@gmail.com>
2021-11-15 09:30:24 +0900

    하윤 1406 update : 패키지이름에 몇주차인지 추가함

---
+++ src/w6_boj1406/ha1406.java
@@ -0,0 +1,21 @@
+package w6_boj1406;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ha1406 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		String str = br.readLine();
+		int M = Integer.parseInt(br.readLine());
+		
+
+	}
+
+}

359b1cf01eff926116c6957cba92c5f4bc236b14
nhy31 <nahay131@gmail.com>
2021-11-16 08:54:09 +0900

    update 하윤

--- src/w6_boj1026/ha1026.java
+++ src/w6_boj1026/ha1026.java
@@ -1,9 +1,38 @@
 package w6_boj1026;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class ha1026 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		int N  = Integer.parseInt(br.readLine()); // N<50
+		
+		//A와 B의 각 원소는 100보다 작거나 같은 음이 아닌 정수이다.
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] A = new int[N];
+		for(int i=0; i<N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(A);
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		int[] B = new int[N];
+		for(int i=0; i<N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(B);
+		
+		int result = 0;
+		for(int i=0; i<N; i++) {
+			result += A[i]*B[N-1-i];
+		}
+		
+		System.out.print(result);
 
 	}
 
--- src/w6_boj1406/ha1406.java
+++ src/w6_boj1406/ha1406.java
@@ -3,6 +3,7 @@ package w6_boj1406;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class ha1406 {
@@ -10,10 +11,45 @@ public class ha1406 {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
+		Stack<String> stack = new Stack<>();
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
+		//이 문자열은 길이가 N이고, 영어 소문자로만 이루어져 있으며, 길이는 100,000을 넘지 않는다
 		String str = br.readLine();
+		
+		//입력할 명령어의 개수를 나타내는 정수 M(1 ≤ M ≤ 500,000)
 		int M = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		//M개의 줄에 걸쳐 입력할 명령어가 순서대로 주어진다
+		for(int i=0; i<M; i++) {
+			String command = st.nextToken();
+			
+			switch(command) {
+			
+				case "L":
+					break;
+				
+					
+				case "D":
+					break;
+				
+					
+				case "B":
+					break;
+				
+					
+				case "P":
+					str += st.nextToken();
+					break;
+				
+			}
+			String s = st.nextToken();
+			
+			
+		}
 		
 
 	}

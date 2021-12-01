7942793eb96d63e59f930015ce7d78caa97d3ed2
sein <continue.deve@gmail.com>
2021-12-01 18:08:12 +0900

    [UPDATE]sein week8 success

--- src/w8_10867/sein10867.java
+++ src/w8_10867/sein10867.java
@@ -1,4 +1,32 @@
 package w8_10867;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class sein10867 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        for(int i = 0; i < N; i++) {
+            if(i > 0 && num[i] == num[i - 1]) {
+                continue;
+            }
+            System.out.print(num[i] + " ");
+        }
+    }
 }
+

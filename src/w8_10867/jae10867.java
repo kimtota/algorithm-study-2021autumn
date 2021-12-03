46800300400cb30aa05271090b51aea9c07da024
jakim09 <jakim09@naver.com>
2021-12-03 20:58:49 +0900

    [update] 재아 8주차 과제

---
+++ src/w8_10867/jae10867.java
@@ -0,0 +1,34 @@
+package w8_10867;
+
+import java.io.*;
+import java.util.*;
+
+public class jae10867 {
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		int [] plusNum=new int[1001];
+		int [] minusNum=new int[1001];
+		String [] strArr=br.readLine().split(" ");
+		StringBuffer strBuffer=new StringBuffer();
+		for(int i=0;i<n;i++) {
+			int num= Integer.parseInt(strArr[i]);
+			if(num>=0) {
+				plusNum[num]=1;
+			}else {
+				minusNum[num*(-1)]=1;
+			}
+		}
+		
+		for(int i=1000;i>=0;i--) {
+			if(minusNum[i]>0) strBuffer.append(-i+" ");
+		}
+		for(int i=0;i<1001;i++) {
+			if(plusNum[i]>0) strBuffer.append(i+" ");
+		}
+		
+		System.out.println(strBuffer);
+		
+	}
+}
